03ceb7beaccdc176d8ed0273ead71f1b8adc29b4
meriam khanfir <meriam.khanfir@ensi-uma.tn>
2022-12-27 21:35:07 +0100

    add_recherche-feature

--- recherche.c
+++ recherche.c
@@ -1,40 +1,46 @@
-//fonction recherche 
-int research (int idrech )
+#include <stdio.h>
+#include <stdlib.h>
+#include"struct.h"
+#include<conio.h>
+int nombre_de_produit(){
+   //calclule le nombre de produit
+int c;
+FILE *f;
+      f = fopen("stock.txt", "r");
+int nbl = 0;
+while((c=fgetc(f)) != EOF)
 {
-	file *F;
-	F = fopen("stock.txt","r");
-	do
-	{
-		fscanf(F,"%d ;%d ;%s ;%d ;%d ;%d ;%d ;%d ;%d ;%d|n",&produit.pa,&produit.pv,&produit.nom,&produit.delai.jour,&produit.delai.mois,&produit.delai.annee,&produit.quant,&produit.fragilite,&produit.id,&produit.importance);
-		fflush(stdin);
-		if (produit.id=idrech)
-		{
-			fclose(F);
-			return 1;
-		}
-	}while(!feof(F));
-
-fclose(F);
-return -1;
+	if(c=='\n')
+		nbl++;
 }
-//recherche et affichage 
-void recherche()
-{
-	int idrech;
-	printf("entrer le numero de serie de produit shouaite de le rechercher|n");
-	scanf("%d",&idrech);
-	file *F;
-	F = fopen("stock.txt","r");
-	do
-	{
-		fscanf(F,"%d ;%d ;%s ;%d ;%d ;%d ;%d ;%d ;%d ;%d|n",&produit.pa,&produit.pv,&produit.nom,&produit.delai.jour,&produit.delai.mois,&produit.delai.annee,&produit.quant,&produit.fragilite,&produit.id,&produit.importance);
-		fflush(stdin);
-		if (idrech==produit.id)
-		{
-			print_produit();
-		}
-	}while(!feof(F));
+ fclose(f);
+return nbl;
 
-fclose(F);
+}
+
+int rech(int idrech)
+{   produit p;
+    FILE *F;
+    int n;
+    F=fopen("stock.txt","r");
+if(F==NULL){
+             printf("Erreur lors de l'ouverture d'un fichier");
+                              exit(1);
+            }
+            n=nombre_de_produit();
+             // lire les donn�es � partie de fichier
+             for(int j=0;j<n;j++){
+fscanf(F, "Nom : %s\tID : %d\tprixachat : %d\tprixvente : %d\tquantite : %d\tfrag : %d\timpor : %d\tdate d'expiration : %d/%d/%d\n", &p.nom, &p.id, &p.pa, &p.pv, &p.quant, &p.fragilite, &p.importance, &p.delai.jj, &p.delai.mm, &p.delai.aa);
+
+if(idrech==p.id){
+  fclose(F);
+  return 1;
+}
+
+            
+
+            }
+      fclose(F);
+      return -1;
 
 }
