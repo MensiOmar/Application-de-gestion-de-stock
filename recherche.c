080627dde978422f7308b0f5168950b2d7827501
MensiOmar <120733398+MensiOmar@users.noreply.github.com>
2022-12-28 22:09:36 +0100

    add history feauture to all app functions

--- recherche.c
+++ recherche.c
@@ -36,11 +36,35 @@ if(idrech==p.id){
   fclose(F);
   return 1;
 }
-
-            
-
             }
       fclose(F);
       return -1;
+
+}
+void recherche_affichage(){
+      produit p;
+    FILE *F;
+    int n,idrech;
+
+    F=fopen("stock.txt","r");
+    printf("donner l'id du produit a recherche");
+    scanf("%d",&idrech);
+    if(F==NULL){
+             printf("Erreur lors de l'ouverture d'un fichier");
+                              exit(1);
+            }
+            n=nombre_de_produit();
+             // lire les donnees a partir de fichier
+             for(int j=0;j<n;j++){
+fscanf(F, "Nom : %s\tID : %d\tprixachat : %d\tprixvente : %d\tquantite : %d\tfrag : %d\timpor : %d\tdate d'expiration : %d/%d/%d\n", &p.nom, &p.id, &p.pa, &p.pv, &p.quant, &p.fragilite, &p.importance, &p.delai.jj, &p.delai.mm, &p.delai.aa);
+
+if(idrech==p.id){
+  fclose(F);
+  printf(F, "Nom : %s\tID : %d\tprixachat : %d\tprixvente : %d\tquantite : %d\tfrag : %d\timpor : %d\tdate d'expiration : %d/%d/%d\n", p.nom, p.id,p.pa,p.pv,p.quant,p.fragilite,p.importance,p.delai.jj,p.delai.mm,p.delai.aa);
+
+}
+            }
+      fclose(F);
+      printf("produit non existant");
 
 }
--- stocker_les_produit.c
+++ stocker_les_produit.c
@@ -1,20 +1,14 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include"struct.h"
-
-
-
-
 void ajout_produit( ){
-
     produit pt;
     int choix;
-
-    
-
-            FILE *f;
+    action act;
+        FILE *f,*l;
     produit p;
-    f = fopen("stock.txt", "a");
+    f = fopen("stock.txt", "a+");
+    l = fopen("historique.txt", "a+");
      if(f==NULL){
          printf("Erreur lors de l'ouverture d'un fichier");
                 exit(1);
@@ -22,15 +16,19 @@ void ajout_produit( ){
         printf("\nDonner un produit :\n ");
         pt=saisie_produit(pt);
         fprintf(f, "Nom : %s\tID : %d\tprixachat : %d\tprixvente : %d\tquantite : %d\tfrag : %d\timpor : %d\tdate d'expiration : %d/%d/%d\n", pt.nom, pt.id,pt.pa,pt.pv,pt.quant,pt.fragilite,pt.importance,pt.delai.jj,pt.delai.mm,pt.delai.aa);
+        act.type=1;
+        act.quant=pt.quant;
+        
+        printf("donner la date de l'ajout de ce produit: \n");
+        act.date_act=saisie_date(act.date_act);
+        printf("donner l'heure de l'ajout de ce produit: \n");
+        act.heure_act=saisie_heure(act.heure_act);
 
-       // fprintf(f,"\n");
-        fclose(f);
+        fprintf(l,"type: %d\tquantite: %d nom du produit: %s\t date d'action: %d/%d/%d\t heure d'action: %d:%d\n",act.type,act.quant,pt.nom,act.date_act.jj,act.date_act.mm,act.date_act.aa,act.heure_act.hh,act.heure_act.mm);
        
-
-
-
+        fclose(f);
+        fclose(l);
 }
-
 
 
 
