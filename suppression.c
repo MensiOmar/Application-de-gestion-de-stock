7a3e451ec4189410cf8ed5f18778421ef3b5c639
MensiOmar <120733398+MensiOmar@users.noreply.github.com>
2022-12-28 22:15:05 +0100

    small review

--- suppression.c
+++ suppression.c
@@ -2,79 +2,6 @@
 #include <stdlib.h>
 #include"struct.h"
 #include<conio.h>
-heure saisie_heure(heure heure){
-    int hh,mm;
-    do{
-        printf("heure: ");
-        scanf("%d",&hh);
-    }while(hh>24||hh<0);
-    do{
-        printf("minutes: ");
-        scanf("%d",&mm);
-    }while(mm>59||mm<0);
-    heure.hh=hh;
-    heure.mm=mm;
-    return heure;
-    
-}
-int nombre_de_produit(){
-   //calclul du  nombre de produit
-int c;
-FILE *f;
-      f = fopen("stock.txt", "r");
-int nbl = 0;
-while((c=fgetc(f)) != EOF)
-{
-	if(c=='\n')
-		nbl++; 
-}
- fclose(f);
-return nbl;
-
-}
-int rech(int idrech)
-{   produit p;
-    FILE *F;
-    int n;
-    F=fopen("stock.txt","r");
-if(F==NULL){
-             printf("Erreur lors de l'ouverture d'un fichier");
-                              exit(1);
-            }
-            n=nombre_de_produit();
-             // lire les donnees a partir de fichier
-             for(int j=0;j<n;j++){
-fscanf(F, "Nom : %s\tID : %d\tprixachat : %d\tprixvente : %d\tquantite : %d\tfrag : %d\timpor : %d\tdate d'expiration : %d/%d/%d\n", &p.nom, &p.id, &p.pa, &p.pv, &p.quant, &p.fragilite, &p.importance, &p.delai.jj, &p.delai.mm, &p.delai.aa);
-
-if(idrech==p.id){
-  fclose(F);
-  return 1;
-}
-            }
-      fclose(F);
-      return -1;
-
-}
-date saisie_date(date date)
-{
-    do{
-        printf("annee: ");
-        scanf("%d",&date.aa);
-    }while(date.aa>2100||date.aa<1900);
-
-    do{
-        printf("mois: ");
-        scanf("%d",&date.mm);
-    }while(date.mm>12||date.mm<1);
-
-    do{
-        printf("jour: ");
-        scanf("%d",&date.jj);
-    }while((((date.mm==1)||(date.mm==3)||(date.mm==5)||(date.mm==7)||(date.mm==8)||(date.mm==10)||(date.mm==12))&&date.jj>31)||(((date.mm==4)||(date.mm==6)||(date.mm==9)||(date.mm==11)||(date.mm==8))&&date.jj>30)||((date.mm==2)&&date.jj>29)||date.jj<1);
-
-    return date;
-}
-
 void supprimerproduit(){
 action act;
 produit p;
@@ -122,10 +49,6 @@ fclose(F);
 remove("stock.txt");
 rename("tempstock.txt","stock.txt");
 printf("suppression Effectue avec Succees");
-}
-
-int main(){
-    supprimerproduit();
 }
 
 
